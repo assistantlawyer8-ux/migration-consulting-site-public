512d4ec3213fc04885b2ca43a4e366e4f684ea79
assistantlawyer8-ux <assistantlawyer8@gmail.com>
2026-08-18 20:26:02 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,15 @@
 import { withAuth } from 'next-auth/middleware';
-export default withAuth({ pages: { signIn: '/admin/login' } });
-export const config = { matcher: ['/admin/:path*', '!/admin/login'] };
+
+export default withAuth(
+  function middleware() {},
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        if (req.nextUrl.pathname.startsWith('/admin/login')) return true;
+        return !!token;
+      },
+    },
+  }
+);
+
+export const config = { matcher: ['/admin/:path*'] };
